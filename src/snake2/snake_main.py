32e7c2965e547a2089aeb65c016a4e057145091f
Edward Tomlins <E176149@ameren.com>
2022-06-02 12:03:45 -0500

    snake moves

--- src/snake2/snake_main.py
+++ src/snake2/snake_main.py
@@ -1,54 +1,6 @@
 import pygame
 import random
 
-#def run():
- #   global w, h
-    
-    # print ('Running the main script!')
-    #pygame.init()
-    #pygame.display.init()
-    #print(pygame.display.get_init())
-    #green = (34,139,34)
-    #othergreen = (0,255,0)
-    #w = 800
-    #h = 800
-    #screen = pygame.display.set_mode((w, h))
-    #snake = pygame.surface(20,20)
-    #screen.fill(green)
-    #pygame.display.update()
-    #game_in_progress = 1
-    #speed = 0
-    #snakewidth = 20
-    #snakeheight = 20
-    
-    #while game_in_progress:
-#        snakewidth += speed
-#        snakeheight += speed
-#        screen.blit(snake, (snakewidth,snakeheight))
-#        pygame.display.update()
-#        for event in pygame.event.get():
-#            speed = 1
-#
-#        if event.type == pygame.KEYDOWN:
-#            if event.key == pygame.K_LEFT:
-#                print('left')
-#            if event.key == pygame.K_RIGHT:
-#                print('right')
-#            if event.key == pygame.K_UP:
-#                print('up')
-#            if event.key == pygame.K_DOWN:
-#                print('down')
-#
-#            if event.key == ord('q'):
-#                game_in_progress = False    
-#    pygame.display.quit()
-#    pygame.quit()
-#    return
-#    
-#    
-#
-#if __name__ == '__main__': #if the user runs "snake_main.py" from terminal
-#    run()
 
 size = 10    # size of the apple (same size as grid spot)
 
@@ -80,6 +32,17 @@ def check_collision(self, x1, y1, x2, y2):
             return True         # returns True if there is a collision
     return False        # returns False if there is no collision
 
+from turtle import width
+import pygame
+
+class Sprite:
+    x = 100
+    y = 100
+    width = 20
+    height = 20
+    #topedge = 100
+    #bottomedge = 100
+
 def run(): 
     global w, h 
 
@@ -99,36 +62,44 @@ def run():
     square.fill((255, 0, 0))
     clock = pygame.time.Clock()
 
-    # initialize apple into the game
-    apple = Apple(screen)
-    apple.draw()
-
     while game_in_progress:        
-
-        screen.blit(square, (Sprite.x, Sprite.y))
+        screen.fill(green) 
+        pygame.draw.rect(screen, othergreen, (Sprite.x, Sprite.y, Sprite.width, Sprite.height))
+        # screen.blit(square, (Sprite.x, Sprite.y))
         # pygame.draw.circle(screen, othergreen, (100,100), 10) #sprite
         # pygame.display.update() 
 
         for event in pygame.event.get(): 
             speed = 1 
 
-            if event.type == pygame.KEYDOWN: 
-                if event.key == pygame.K_LEFT or event.key == ord('a'): 
-                    Sprite.x -= 10
-                    print('left')
+        if event.type == pygame.KEYDOWN: 
+            if event.key == pygame.K_LEFT or event.key == ord('a'): 
+                Sprite.x -= 10                    
 
-                if event.key == pygame.K_RIGHT or event.key == ord('d'):
-                    Sprite.x += 10 
-                    print('right') 
+            if event.key == pygame.K_RIGHT or event.key == ord('d'):
+                Sprite.x += 10 
 
-                if event.key == pygame.K_UP or event.key == ord('w'): 
-                    Sprite.y -= 10
-                    print('up') 
+            if event.key == pygame.K_UP or event.key == ord('w'): 
+                Sprite.y -= 10
 
-                if event.key == pygame.K_DOWN or event.key == ord('s'): 
-                    Sprite.y += 10
-                    print('down') 
 
+            if event.key == pygame.K_DOWN or event.key == ord('s'): 
+                Sprite.y += 10
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == ord('a'): 
+                Sprite.x -= 10                    
+
+            if event.key == pygame.K_RIGHT or event.key == ord('d'):
+                Sprite.x += 10 
+
+            if event.key == pygame.K_UP or event.key == ord('w'): 
+                Sprite.y -= 10
+
+            if event.key == pygame.K_DOWN or event.key == ord('s'): 
+                Sprite.y += 10
+   
+        
         pygame.display.update() 
         clock.tick(60)
 
